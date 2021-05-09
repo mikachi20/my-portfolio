3b5e5de33b80ec7474e527d5fcbc12c1e988a906
Mikasa <tomo.400m@icloud.com>
2021-05-09 09:33:32 +0900

    ポートフォリオ

--- main.js
+++ main.js
@@ -1,40 +1,77 @@
-let hero = document.querySelector("#hero");
-let backgroundWords = document.querySelector("#background-words");
-let formInput = document.querySelector("#myForm input");
+const button11 = document.getElementById("button-one1");
+const button21 = document.getElementById("button-two1");
+const button31 = document.getElementById("button-three1");
+const button12 = document.getElementById("button-one2");
+const button22 = document.getElementById("button-two2");
+const button32 = document.getElementById("button-three2");
+const memoInput = document.getElementById("memo-input");
+const addButton = document.getElementById("add-button");
+const memoContainer = document.getElementById("memo-container");
+const replyContainer = document.getElementById("reply-container");
 
-let colors = [
-  "yellowgreen",
-  "darkcyan",
-  "crimson",
-  "cornflowerblue",
-  "gold",
-  "goldenrod",
-  "lightgoldenrodyellow",
-  "#eee",
+button11.onclick = function () {
+  answer1.textContent = "正解！でも選択肢全部正解！";
+};
+
+button21.onclick = function () {
+  answer1.textContent = "正解！でも選択肢全部正解！";
+};
+
+button31.onclick = function () {
+  answer1.textContent = "正解！でも選択肢全部正解！";
+};
+
+button12.onclick = function () {
+  answer2.textContent = "ぶっぶー";
+};
+
+button22.onclick = function () {
+  answer2.textContent = "ひどい😭";
+};
+
+button32.onclick = function () {
+  answer2.textContent = "よくわかってる♡";
+};
+
+let rand = 0;
+const replies = [
+  "こんにちは！良い天気だね！",
+  "昨日の晩ご飯は何だった？",
+  "今日も一日頑張ろう！",
+  "LINE来るの待ってたよ！",
+  "それはすごい！",
 ];
 
-hero.addEventListener("mouseover", function () {
-  let newColor = colors[Math.floor(Math.random() * colors.length)];
-  formInput.style.color = newColor;
-  backgroundWords.style.color = newColor;
-  let div = document.createElement("div");
-  createIndividualLetters(div);
-});
+addButton.onclick = function () {
+  addCard();
+  rep();
+};
 
-formInput.addEventListener("keyup", function () {
-  let divs = document.querySelectorAll("#background-words div");
-  for (let div of divs) {
-    createIndividualLetters(div);
-  }
-});
+const addCard = function () {
+  const card = document.createElement("div");
+  card.textContent = memoInput.value;
+  memoContainer.append(card);
+  memoInput.value = "";
+  return card;
+};
 
-function createIndividualLetters(div) {
-  let inputValue = formInput.value;
-  div.innerHTML = "";
-  for (let letter of inputValue) {
-    let span = document.createElement("span");
-    span.innerHTML = letter;
-    div.appendChild(span);
+const rep = function () {
+  let rand = random();
+  const re = document.createElement("div");
+  if (rand <= 0.2) {
+    re.textContent = replies[0];
+  } else if (rand <= 0.4) {
+    re.textContent = replies[1];
+  } else if (rand <= 0.6) {
+    re.textContent = replies[2];
+  } else if (rand <= 0.8) {
+    re.textContent = replies[3];
+  } else if (rand <= 1) {
+    re.textContent = replies[4];
   }
-  backgroundWords.appendChild(div);
-}
+  replyContainer.append(re);
+};
+
+const random = function () {
+  return Math.random();
+};
